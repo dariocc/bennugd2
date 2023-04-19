d6bd1a738de9b4994bb400e94d2002b5a1af3ac9
Darío Cutillas Carrillo <git@dario.nu>
2023-04-19 10:04:31 +0200

    Make it possible to build libmod_tmx

--- modules/libmod_tmx/libmod_tmx.c
+++ modules/libmod_tmx/libmod_tmx.c
@@ -7,6 +7,8 @@
 #include "files.h"
 #include "xctype.h"
 #include "xstrings.h"
+#include "m_map.h"
+#include "g_grlib.h"
 
 #include "dlvaracc.h"
 
@@ -40,10 +42,23 @@ static int64_t libmod_tmx_unload_map(INSTANCE * my, int64_t * params) {
     tmx_map_free(map);
 }
 
-static void* tex_loader(const char *path) {
+typedef struct _tex {
+    int64_t graph;
+} TMX_TEX;
+
+static void* tex_load(const char *path) {
+    TMX_TEX * tex = malloc(sizeof(TMX_TEX));
+    tex->graph = gr_load_img(path);
+    return tex;
+}
+
+static void* tex_free(void * handler) {
+    TMX_TEX * tex = (TMX_TEX *)handler;
+    return (void *) grlib_unload_map(0, tex->graph);
 }
 
 void __bgdexport( libmod_tmx, module_initialize )() {
+    tmx_img_load_func = tex_load;
 }
 
 void __bgdexport( libmod_tmx, module_finalize )() {
