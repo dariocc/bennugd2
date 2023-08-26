e37c1cd66904f8425c5dbb7ac47b8ca38beccbc0
Darío Cutillas Carrillo <git@dario.nu>
2023-08-26 13:00:16 +0200

    Add play_tmx_example to set_vars

--- set_vars.sh
+++ set_vars.sh
@@ -1,3 +1,8 @@
 LD_LIBRARY_PATH=~/Projects/bennugd2/build/build-linux-gnu/bin
 export LD_LIBRARY_PATH
 PATH=~/Projects/bennugd2/build/build-linux-gnu/bin:$PATH
+
+play_tmx_example() {
+    bgdc 11-tmx.prg && bgdi 11-tmx.dcb
+}
+export -f play_tmx_example
